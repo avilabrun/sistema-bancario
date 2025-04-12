64c8c79151bd94722c8d68849121f7984e044b44
Bruno <avilabruno@live.com>
2025-04-12 20:30:55 -0300

    inclusão das funções de saque, depósito, extrato e consulta de saldo

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,11 +1,11 @@
 menu = """
-
 Olá!
 Informe o serviço desejado entre as opções:
 
     1 - Extrato
     2 - Saque
     3 - Depósito
+    9 - Saldo
     0 - Sair
 
 => """
@@ -21,16 +21,44 @@ while True:
     opcao = int(input(menu))
     
     if opcao == 1: # Extrato
-        print("s")
+        print(extrato)
+
+        print(f"Saldo:         R$ {saldo:.2f}\n")
 
     elif opcao == 2: # Saque
-        valor_saque = float(input("Informe o valor desejado: "))
+        if numero_saques == LIMITE_SAQUES: # Testa quandidade de saques disponíveis
+            print("Número de saques diários excedido.")
         
+        else:
+            valor_saque = float(input("Informe o valor desejado: "))
+            
+            if valor_saque <= 0:
+                print("Valor informado inválido para operações de saque!")
+
+            else:
+                if valor_saque > saldo: # Testa saldo suficente
+                    print("Saldo insuficiente para operação")
+                
+                elif valor_saque > limite: # Testa limite de saque por operação
+                    print("Valor informado acima do limite permitido para operações de saque!")
+
+                else: # Realiza saque se todas as condições forem atendidas
+                    saldo -= valor_saque # Incrementa saldo
+                    numero_saques += 1 # Incrementa saldo
+                    extrato += f"Saque:         R$ {valor_saque:.2f}\n" # Registra extrato
 
     elif opcao == 3: # Depósito
         valor_deposito = int(input("Informe o valor a ser depositado: "))
+        if valor_deposito > 0: # Testa valor positivo para depósito
+            saldo += valor_deposito # Incrementa saldo
+            extrato += f"Depósito:      R$ {valor_deposito:.2f}\n" # Registra extrato
         
+        else: # Mensagem de erro para valor de depósito inválido
+            print("Valor informado inválido para operação de depósito.")
 
+    elif opcao == 9: # Imprime o saldo atual
+        print(f"Saldo:         R$ {saldo:.2f}\n")
+    
     elif opcao == 0: # Sair
         print("Obrigado por usar nossos serviços!")
         break
